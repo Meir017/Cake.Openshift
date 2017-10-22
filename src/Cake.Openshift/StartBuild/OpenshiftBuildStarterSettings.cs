f5360fd9cff12320597c650d41f765f1bd524cdc
Meir Blachman <meirblachman@protonmail.com>
2017-10-22 23:08:53 +0300

    added the command start-build

---
+++ src/Cake.Openshift/StartBuild/OpenshiftBuildStarterSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace Cake.Openshift.StartBuild
+{
+    /// <summary>
+    /// Contains settings used by <see cref="OpenshiftBuildStarter" />.
+    /// </summary>
+    public class OpenshiftBuildStarterSettings : OpenshiftSettings
+    {
+        /// <summary>
+        /// Gets or sets the source code commit identifier the build should use.
+        /// </summary>
+        /// <remarks>requires a build based on a Git repository</remarks>
+        public string Commit { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to watch its logs until it completes or fails.
+        /// </summary>
+        public bool Follow { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to wait for a build to complete and exit with a non-zero return code if the build fails.
+        /// </summary>
+        public bool Wait { get; set; }
+    }
+}
---
+++ test/Cake.Openshift.Tests/StartBuild/OpenshiftBuildStarterFixture.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Openshift.StartBuild;
+
+namespace Cake.Openshift.Tests.StartBuild
+{
+    public class OpenshiftBuildStarterFixture : OpenshiftFixture<OpenshiftBuildStarterSettings>
+    {
+        public string BuildConfig { get; set; }
+
+        protected override void RunTool()
+        {
+            var buildStarter = new OpenshiftBuildStarter(FileSystem, Environment, ProcessRunner, Tools);
+            buildStarter.Run(BuildConfig, Settings);
+        }
+    }
+}
